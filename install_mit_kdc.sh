0f1a3347452f2e023bb5480eef78c05f84b34d76
sprakashhw <43625718+sprakashhw@users.noreply.github.com>
2021-06-24 13:17:25 -0500

    Update install_mit_kdc.sh

--- install_mit_kdc.sh
+++ install_mit_kdc.sh
@@ -1,6 +1,6 @@
 PUBLIC_HOSTNAME=`curl http://169.254.169.254/latest/meta-data/public-hostname`
 yum install -y krb5-server krb5-libs krb5-workstation
-mv /home/centos/krb5.conf /etc/krb5.conf
+mv conf/krb5.conf /etc/krb5.conf
 sed -i "s/Yourhostname/$PUBLIC_HOSTNAME/g" /etc/krb5.conf
 kdb5_util create -s -P Databricks@2021
 systemctl start krb5kdc
