fdd97a7b535b40bf0ea9a0c0f1ec22a4bb7e0e80
sprakashhw <43625718+sprakashhw@users.noreply.github.com>
2021-06-24 13:11:50 -0500

    Create install_mit_kdc.sh

---
+++ install_mit_kdc.sh
@@ -0,0 +1,10 @@
+PUBLIC_HOSTNAME=`curl http://169.254.169.254/latest/meta-data/public-hostname`
+yum install -y krb5-server krb5-libs krb5-workstation
+mv /home/centos/krb5.conf /etc/krb5.conf
+sed -i "s/Yourhostname/$PUBLIC_HOSTNAME/g" /etc/krb5.conf
+kdb5_util create -s -P Databricks@2021
+systemctl start krb5kdc
+systemctl enable krb5kdc
+systemctl enable kadmin
+systemctl restart kadmin
+kadmin.local  -q "addprinc -pw Databricks@2021  admin/admin@EXAMPLE.COM"
