667ab6be11c16c2bbf7c65f7fd37d3ede05aad49
sprakashhw <43625718+sprakashhw@users.noreply.github.com>
2021-06-24 19:03:38 -0500

    Update install_mit_kdc.sh

--- install_mit_kdc.sh
+++ install_mit_kdc.sh
@@ -1,4 +1,5 @@
 INTERNAL_HOSTNAME=`hostname -f`
+PUBLIC_HOSTNAME=`curl http://169.254.169.254/latest/meta-data/public-hostname`
 yum install -y krb5-server krb5-libs krb5-workstation
 mv conf/krb5.conf /etc/krb5.conf
 sed -i "s/Yourhostname/$INTERNAL_HOSTNAME/g" /etc/krb5.conf
@@ -8,4 +9,5 @@ systemctl enable krb5kdc
 systemctl enable kadmin
 systemctl restart kadmin
 kadmin.local  -q "addprinc -pw Databricks@2021  admin/admin@EXAMPLE.COM"
+kadmin.local  -q "addprinc -pw Databricks@2021  kafka/$PUBLIC_HOSTNAME@EXAMPLE.COM"
 systemctl restart kadmin
